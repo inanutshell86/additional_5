4030113a7cf3bd01e42c33db00904ca0ecdb42f4
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-09-29 11:54:13 +0300

    Add solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const length = str.length;
+  const stack = [];
+  const bracketsArr = [...bracketsConfig];
+  const openBracketArr = [];
+  const closeBracketArr = [];
+
+  if (length % 2 !== 0) {
+    return false;
+  }
+
+  bracketsArr.forEach(b => {
+    openBracketArr.push(b[0]);
+    closeBracketArr.push(b[1]);
+  });
+
+  for (let i = 0; i < length; i++) {
+    if (openBracketArr.includes(str[i])) {
+      if (closeBracketArr.includes(str[i]) && stack.includes(str[i])) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    }
+
+    else if (closeBracketArr.includes(str[i]) && stack[stack.length - 1] === openBracketArr[closeBracketArr.indexOf(str[i])]) {
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
 }
